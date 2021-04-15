d4461860a2f6a13f62ccf3f8439341ed2c208dd0
shashikumar2 <shashikumarbb@gmail.com>
2021-04-15 13:52:31 +0530

    updated

--- client/src/components/service/List.js
+++ client/src/components/service/List.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import { connect } from 'react-redux'
+import {Link} from 'react-router-dom'
+import {connect} from 'react-redux'
 import moment from 'moment'
 import '../../App.css'
 import {startGetServices, startDeleteService, startPutService} from '../../actions/servicesAction'
